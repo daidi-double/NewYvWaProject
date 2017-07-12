766807a7bcd4e17bd46160961ab214a5293c1799
double <double@yuwabao.cn>
2017-07-12 21:06:13 +0800

    补充缺失文件

--- YuWa/Foundation/ThirdTools/color/FontsAndColors.h
+++ YuWa/Foundation/ThirdTools/color/FontsAndColors.h
@@ -14,13 +14,6 @@
 #define kBaseColor @"#f4921d"
 
 
-
-//FONT_CN   中文48号字体
-//Helvetica-Bold     PingFang SC
-#define FONT_CN_48 [UIFont fontWithName:@"Helvetica-Bold" size:24]
-
-
-
 //FONT_EN   英文48号字体
 
 #define FONT_EN_48 [UIFont fontWithName:@"San Francisco Text" size:24]
