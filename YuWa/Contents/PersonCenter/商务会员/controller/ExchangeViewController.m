8ffbe1914f2e19642aa09b8f2d7919f2b97d07a3
double <double@yuwabao.cn>
2017-08-08 10:45:10 +0800

    调整

--- YuWa/Contents/PersonCenter/商务会员/controller/ExchangeViewController.m
+++ YuWa/Contents/PersonCenter/商务会员/controller/ExchangeViewController.m
@@ -29,7 +29,7 @@
     [self.exchangeGradeTextField addTarget:self action:@selector(textChange:) forControlEvents:UIControlEventEditingChanged];
   
     self.scoreLabel = [self.view viewWithTag:3];
-    self.scoreLabel.text = [NSString stringWithFormat:@"当前兑换比例为%@,可兑换为人民币金额为:0元",self.pay_scale];
+    self.scoreLabel.text = [NSString stringWithFormat:@"当前兑换比例为%@,可兑换人民币金额为:0元",self.pay_scale];
     self.exchangeGradeTextField.keyboardType = UIKeyboardTypeNumbersAndPunctuation;
     
 
@@ -44,7 +44,7 @@
     CGFloat scoreNum = [scoreAry[1] floatValue];
     NSString * money = [NSString stringWithFormat:@"%f",[_canUseGrade floatValue] * scoreNum];
     
-    self.scoreLabel.text = [NSString stringWithFormat:@"当前兑换比例为%@,可兑换为人民币金额为:%@元",self.pay_scale,money];
+    self.scoreLabel.text = [NSString stringWithFormat:@"当前兑换比例为%@,可兑换人民币金额为:%@元",self.pay_scale,money];
 }
 //确认兑换
 - (IBAction)suerExchangeBtnAction:(UIButton *)sender {
@@ -65,7 +65,7 @@
     CGFloat scoreNum = [scoreAry[1] floatValue];
     NSString * money = [NSString stringWithFormat:@"%f",[score floatValue] * scoreNum];
     
-    self.scoreLabel.text = [NSString stringWithFormat:@"当前兑换比例为%@,可兑换为人民币金额为:%@元",self.pay_scale,money];
+    self.scoreLabel.text = [NSString stringWithFormat:@"当前兑换比例为%@,可兑换人民币金额为:%@元",self.pay_scale,money];
 
 }
 - (void)showKeyboard {
